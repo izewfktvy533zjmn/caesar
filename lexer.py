38cc059f95528c5a430817774da75d66e20ed45e
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-02-14 20:38:22 +0900

    Update Lexer.read() and Lexer.close()
    I changed Lexer.read() so that read from file.
    And I added Lexer.close() to close opend file.

--- lexer.py
+++ lexer.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+
 class Lexer:
-    def __init__(self):
-        self.line = ""
+    def __init__(self, fp):
+        self.lin = fp
 
 
-    def read(self, input_str):
-        self.line = input_str
+    def read(self):
+        self.line = self.lin.readline()
+    
+
+    def close(self):
+        self.lin.close()
 
